7e2845409b82777cf2515b112381ff6e1a41abd1
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-31 11:03:59 -0600

    Update sort method to use helper methods, add guard clause to sort whole_suit

--- lib/deck.rb
+++ lib/deck.rb
@@ -9,13 +9,77 @@ class Deck
     @cards.length
   end
 
-  def sort_by_suit
-    @cards.each_con do |card1, card2|
-      require "pry"; binding.pry
+  def sort_cards_by_suit(cards, suit)
+    suit_sorted_cards = []
+    cards.each do |card|
+      suit_sorted_cards << card if card.suit == suit
+    end
+    suit_sorted_cards
+  end
+
+  def sort_into_suits(suits)
+    suits.reduce({}) do |acc, suit|
+      acc[suit] = sort_cards_by_suit(@cards, suit)
+      acc
+    end
+  end
+
+  def sort_cards_by_value(cards)
+    cards.reduce({}) do |acc, card|
+      if acc[:face_cards].nil? and card.value.length > 2
+        acc[:face_cards] = [card]
+      elsif !acc[:face_cards].nil? and card.value.length > 2
+        acc[:face_cards] << card
+      elsif acc[:number_cards].nil? and card.value.length < 2
+        acc[:number_cards] = [card]
+      elsif !acc[:number_cards].nil? and card.value.length < 2
+        acc[:number_cards] << card
+      end
+      acc
+    end
+  end
+
+  def sort_by_face_cards(cards)
+    sorted_face_cards = []
+    cards.each do |card|
+      sorted_face_cards << card if card.value == "Ace"
+    end
+    cards.each do |card|
+      sorted_face_cards << card if card.value == "King"
+    end
+    cards.each do |card|
+      sorted_face_cards << card if card.value == "Queen"
+    end
+    cards.each do |card|
+      sorted_face_cards << card if card.value == "Jack"
+    end
+    sorted_face_cards
+  end
+
+  def sort_cards_by_number(cards)
+    cards.sort_by do |card|
+      card.value.to_i
+    end.reverse
+  end
+
+  def sort_whole_suit(cards)
+    if !(cards == [])
+      sorted_cards = sort_cards_by_value(cards)
+      face_cards = sort_by_face_cards(sorted_cards[:face_cards])
+      number_cards =  sort_cards_by_number(sorted_cards[:number_cards])
+      face_cards + number_cards
+    else
+      []
     end
   end
 
   def sort
-
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    sorted = sort_into_suits(suits)
+    spades = sort_whole_suit(sorted["Spades"])
+    hearts = sort_whole_suit(sorted["Hearts"])
+    clubs = sort_whole_suit(sorted["Clubs"])
+    diamonds = sort_whole_suit(sorted["Diamonds"])
+    spades + hearts + clubs + diamonds
   end
 end
--- test/deck_test.rb
+++ test/deck_test.rb
@@ -10,7 +10,11 @@ class DeckTest < Minitest::Test
     @card_3 = Card.new("5", "Diamonds")
     @card_4 = Card.new("Jack", "Clubs")
     @card_5 = Card.new("Ace", "Diamonds")
-    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5 ]
+    @card_6 = Card.new("4", "Hearts")
+    @card_7 = Card.new("5", "Hearts")
+    @card_8 = Card.new("6", "Hearts")
+    @card_9 = Card.new("Queen", "Hearts")
+    @cards = [@card_1, @card_2, @card_3, @card_4, @card_5, @card_6, @card_7, @card_8, @card_9]
     @deck = Deck.new(@cards)
   end
 
@@ -20,14 +24,54 @@ class DeckTest < Minitest::Test
   end
 
   def test_it_can_count_cards
-    assert_equal 5, @deck.count
+    assert_equal 9, @deck.count
   end
 
   def test_it_can_sort_cards_by_suit
-
+    expected = [@card_1, @card_6, @card_7, @card_8, @card_9]
+    assert_equal expected, @deck.sort_cards_by_suit(@cards, "Hearts")
   end
-  def test_it_can_sort_cards
-    expected = [@card_1, @card_3, @card_4, @card_5, @card_2]
+
+  def test_it_can_sort_all_deck_into_all_suits
+    suits = ["Spades", "Hearts", "Clubs", "Diamonds"]
+    expected = {
+      "Spades" => [],
+      "Hearts" => [@card_1, @card_6, @card_7, @card_8, @card_9],
+      "Clubs" => [@card_2, @card_4],
+      "Diamonds" => [@card_3, @card_5]
+    }
+    assert_equal expected, @deck.sort_into_suits(suits)
+  end
+
+  def test_it_can_sort_by_face_cards
+    assert_equal [@card_4], @deck.sort_by_face_cards([@card_4, @card_2])
+    assert_equal [@card_5, ], @deck.sort_by_face_cards([@card_5, @card_3])
+  end
+
+
+  def test_it_can_sort_cards_by_number
+    test = [@card_1, @card_6, @card_7, @card_8]
+    expected = [@card_8, @card_7, @card_6, @card_1]
+    assert_equal expected, @deck.sort_cards_by_number(test)
+  end
+
+  def test_it_can_sort_all_cards_of_one_suit
+    card_10 = Card.new("King", "Hearts")
+    test = [@card_1, @card_6, @card_7, @card_8, @card_9, card_10]
+    expected = [card_10, @card_9, @card_8, @card_7, @card_6, @card_1]
+    assert_equal expected, @deck.sort_whole_suit(test)
+  end
+
+  def test_it_can_sort_deck
+    expected = [@card_9,
+                @card_8,
+                @card_7,
+                @card_6,
+                @card_1,
+                @card_4,
+                @card_2,
+                @card_5,
+                @card_3]
     assert_equal expected, @deck.sort
   end
 end
